56cbe43c891370d016c952fdc694ed29b8734b98
Aarmil Sadarani <115003760+Aarmil1234@users.noreply.github.com>
2025-04-28 00:19:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const connectDB = require('./config/db');
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const auth = require('./middleware/auth');
-
+const cors = require('cors'); // Import cors
 const PORT = process.env.PORT || 5000;
 
 // Connect to DB
 connectDB();
+
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
